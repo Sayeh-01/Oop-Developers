b091601550b434d012e820bb089d472b56b4e064
PANDA-179 <marwanelserafy99@icloud.com>
2025-04-21 21:35:32 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("b7bk ya panda");
-        System.out.println("fih takadom")
+        System.out.println("fih takadom");
 
 
     }
