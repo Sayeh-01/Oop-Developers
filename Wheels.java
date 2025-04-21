49699fa4e9cbb8e17645ecfa05771d3010263383
Sayeh-01 <youssefislamsayeh@gmail.com>
2025-04-21 21:46:31 +0200

    Update Wheels.java

--- Wheels.java
+++ Wheels.java
@@ -1,10 +1,1 @@
-public class Wheels extends Car{
-    String wheelsId;
-    double pressure;
 
-    public Wheels(int carNo, int maxSpeed, String color, String wheelsId, double pressure) {
-        super(carNo, maxSpeed, color);
-        this.wheelsId = wheelsId;
-        this.pressure = pressure;
-    }
-}
