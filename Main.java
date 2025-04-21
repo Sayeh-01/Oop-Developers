b22c18db2b3e3519a5947c738175e40a906eddf4
yassineyasser5 <yassineyasser5@gmail.com>
2025-04-21 21:37:33 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("b7bk ya panda");
         System.out.println("fih takadom");
+            System.out.println("fih takadom");
 
 
     }
