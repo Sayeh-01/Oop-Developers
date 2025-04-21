2b1eb8c9c68c79e8692ee627567b337f81915981
Sayeh-01 <youssefislamsayeh@gmail.com>
2025-04-21 21:46:52 +0200

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,16 +1,1 @@
-public class Staff  {
-    String specialization;
-    int workingDays=5;
-    double  workingHours;
-    double salary;
 
-    Staff(String Specialization ,String name){
-        
-        this.specialization=Specialization;
-    }
-
-    void CalculateSalary(){
-        this.salary= workingHours*100;
-    }
-
-}
